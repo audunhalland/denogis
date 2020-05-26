c92789801c9097b09812e00047f962bfc5282ab0
Audun Halland <audun.halland@gmail.com>
2020-05-26 03:02:45 +0200

    Insert a polygon into the database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,46 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
+// https://spatialreference.org/ref/epsg/4326/
+// The projection also used in GeoJSON
+const SRID = "4326";
+
+interface Geometry {
+  type: "Polygon";
+  coordinates: Array<Array<[number, number]>>;
+}
+
 async function recreateTables(client: Client) {
-  const meh = await client.query("DROP TABLE IF EXISTS test;");
-  console.log("meh: ", meh);
+  await client.query("DROP TABLE IF EXISTS test;");
+  await client.query(`
+  CREATE TABLE test (
+    id Text,
+    polygon geometry(POLYGON, ${SRID})
+  )
+  `);
   console.log("migration done");
+}
+
+function singlePolygon(coords: Array<[number, number]>): Geometry {
+  return {
+    type: "Polygon",
+    coordinates: [coords],
+  };
+}
+
+function geometryToWKT(geometry: Geometry): string {
+  const fmt = geometry.coordinates.map(
+    (polygons) => `(${polygons.map((coord) => coord.join(" ")).join(",")})`,
+  ).join(",");
+  return `SRID=${SRID};POLYGON(${fmt})`;
+}
+
+async function populate(client: Client) {
+  const geometry = singlePolygon([[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]);
+  await client.query(
+    "INSERT INTO test (id, polygon) VALUES ($1, $2)",
+    "test",
+    geometryToWKT(geometry),
+  );
 }
 
 async function main() {
@@ -16,8 +53,8 @@ async function main() {
   });
   await client.connect();
   try {
-    console.log("yo");
     await recreateTables(client);
+    await populate(client);
   } finally {
     await client.end();
   }
